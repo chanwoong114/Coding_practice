920d4f5a4d71382136c8868a39e72af413976242
chanwoong114 <cksdnd10001@gmail.com>
2023-01-31 23:09:53 +0900

    23/01/31-home

--- baekjoon/18. cumulative_sum/구간_합_구하기5.py
+++ baekjoon/18. cumulative_sum/구간_합_구하기5.py
@@ -1,4 +1,22 @@
+import copy 
+
 n, m = map(int, input().split())
 
-print(n//m)
-print(n%m)
+lst = [list(map(int, input().split())) for _ in range(n)]
+section = [list(map(int, input().split())) for _ in range(m)]
+lst_origin = copy.deepcopy(lst)
+
+for i in range(n):
+    for j in range(1, n):
+        lst[i][j] += lst[i][j-1]
+
+for i in range(m):
+    sum_lst = 0
+    for j in range(section[i][0]-1, section[i][2]):
+        if lst[j][section[i][3]-1] == lst[j][section[i][1]-1]:
+            sum_lst += lst_origin[j][section[i][3]-1]
+        elif section[i][1]==1:
+            sum_lst += lst[j][section[i][3]-1]
+        else:
+            sum_lst += lst[j][section[i][3]-1]-lst[j][section[i][1]-2]
+    print(sum_lst)
