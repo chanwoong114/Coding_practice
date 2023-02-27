75eb19d12328fcbb2f005f679534bfa9d4f20672
chanwoong114 <cksdnd10001@gmail.com>
2023-02-27 23:42:13 +0900

    23/02/27-home

--- algorithm/6. Start/Start1/등산로_조성.py
+++ algorithm/6. Start/Start1/등산로_조성.py
@@ -8,46 +8,44 @@ for test_case in range(1, T+1):
     n, k = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(n)]
 
-    def bfs(a, b, c):
+    high = 0
+    for i in range(n):
+        for j in range(n):
+            if high < board[i][j]:
+                high = board[i][j]
+
+    def bfs(a, b, c, d, e):
         visited = [[[[] for _ in range(n)] for _ in range(n)] for _ in range(2)]
         queue = deque()
-        queue.append((a, b, c))
+        queue.append((a, b, c, d))
         visited[0][a][b].append((a, b))
 
         while queue:
-            x, y, z = queue.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
+            x, y, z, h = queue.popleft()
+            for ii in range(4):
+                nx = x + dx[ii]
+                ny = y + dy[ii]
 
                 if nx < 0 or nx >= n or ny < 0 or ny >= n:
                     continue
 
-                if board[nx][ny] > board[x][y] and (nx, ny) not in visited[z][x][y]:
-                    if len(visited[z][x][y]) >= len(visited[z][nx][ny]):
+                if board[nx][ny] < h and (nx, ny) not in visited[z][x][y]:
+                    if len(visited[z][x][y]) >= len(visited[z][nx][ny])-1:
                         visited[z][nx][ny] = []
                         for abc in visited[z][x][y]:
                             visited[z][nx][ny].append(abc)
                         visited[z][nx][ny].append((nx, ny))
-                        queue.append((nx, ny, z))
+                        queue.append((nx, ny, z, board[nx][ny]))
 
-                if board[nx][ny] + k > board[x][y] and z == 0:
-                    if len(visited[0][x][y]) <= 1:
-                        if (nx, ny) not in visited[z][x][y]:
-                            if len(visited[1][x][y]) >= len(visited[0][nx][ny]):
-                                visited[1][nx][ny] = []
-                                for abc in visited[0][x][y]:
-                                    visited[1][nx][ny].append(abc)
-                                visited[1][nx][ny].append((nx, ny))
-                                queue.append((nx, ny, 1))
-                    elif board[visited[0][x][y][-2][0]][visited[0][x][y][-2][1]] < board[x][y] - k:
-                        if (nx, ny) not in visited[z][x][y]:
-                            if len(visited[1][x][y]) >= len(visited[0][nx][ny]):
-                                visited[1][nx][ny] = []
-                                for abc in visited[0][x][y]:
-                                    visited[1][nx][ny].append(abc)
-                                visited[1][nx][ny].append((nx, ny))
-                                queue.append((nx, ny, 1))
+                if board[nx][ny] - e < h and z == 0:
+                    if (nx, ny) not in visited[z][x][y]:
+                        if len(visited[0][x][y]) >= len(visited[1][nx][ny]):
+                            visited[1][nx][ny] = []
+                            for abc in visited[0][x][y]:
+                                visited[1][nx][ny].append(abc)
+                            visited[1][nx][ny].append((nx, ny))
+                            queue.append((nx, ny, 1, board[nx][ny]-e))
+
         return visited
 
     def len_check():
@@ -57,18 +55,17 @@ for test_case in range(1, T+1):
                 for ccc in range(n):
                     if len(visited[aaa][bbb][ccc]) > max_len:
                         max_len = len(visited[aaa][bbb][ccc])
+
         return max_len
 
     res = 0
     for i in range(n):
         for j in range(n):
-            visited = bfs(i, j, 0)
-            for ac in visited:
-                for bc in ac:
-                    print(*bc)
-            print()
-            a = len_check()
-            if res < a:
-                res = a
+            for ll in range(1, k+1):
+                if board[i][j] == high:
+                    visited = bfs(i, j, 0, high, ll)
+                    a = len_check()
+                    if res < a:
+                        res = a
 
     print(f'#{test_case}', res)
--- baekjoon/28. shortest path/숨바꼭질3.py
+++ baekjoon/28. shortest path/숨바꼭질3.py
@@ -1,3 +1,26 @@
+from collections import deque
+
 n, k = map(int, input().split())
 
-graph = [[] for _ in range(k+2)]
+graph = [[] for _ in range(100001)]
+visited = [0] * 100001
+for i in range(100001):
+    graph[i].append((1, i-1))
+    graph[i].append((1, i+1))
+    graph[i].append((0, i*2))
+
+def bfs():
+    queue = deque()
+    queue.append(n)
+    visited[n] = 1
+    while queue:
+        x = queue.popleft()
+        if x == k:
+            return visited[k] - 1
+        for w, j in graph[x]:
+            if 0 <= j < 100001:
+                if not visited[j] or visited[j] > visited[x] + w:
+                    visited[j] = visited[x] + w
+                    queue.append(j)
+
+print(bfs())
