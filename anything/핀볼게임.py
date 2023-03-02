36d1ff15b8bd2a8da20f53145903797ad1a171bb
chanwoong114 <cksdnd10001@gmail.com>
2023-03-03 01:20:57 +0900

    23/03/02-home

--- anything/핀볼게임.py
+++ anything/핀볼게임.py
@@ -3,7 +3,7 @@ dy = [0, 1, 0, -1]
 
 turn = [(),
         (1, 3, 0, 2),
-        (1, 3, 2, 0),
+        (2, 3, 1, 0),
         (2, 0, 3, 1),
         (3, 2, 0, 1),
         (2, 3, 0, 1)]
@@ -14,28 +14,29 @@ def play(a, b):
         na = a + dx[d]
         nb = b + dy[d]
         if na < 0 or na >= n or nb < 0 or nb >= n:
+            res.append(1)
             continue
 
         cnt = 0
         while True:
-            if board[na][nb] == -1 or (na == a and nb == b):
-                res.append(cnt)
-                break
             if na == -1 or na == n or nb == -1 or nb == n:
                 d = (d + 2) % 4
                 cnt += 1
 
-            if 1 <= board[na][nb] <= 5:
+            elif board[na][nb] == -1 or (na == a and nb == b):
+                res.append(cnt)
+                break
+
+            elif 1 <= board[na][nb] <= 5:
                 cnt += 1
                 d = turn[board[na][nb]][d]
 
-            if board[na][nb] > 5:
+            elif board[na][nb] > 5:
                 na, nb = dic[(na, nb)]
 
             na += dx[d]
             nb += dy[d]
 
-        res.append(cnt)
 
 
 T = int(input())
@@ -55,11 +56,11 @@ for test_case in range(1, T + 1):
         if white[i]:
             dic[white[i][0]] = white[i][1]
             dic[white[i][1]] = white[i][0]
-    print(dic)
 
     for i in range(n):
         for j in range(n):
             if not board[i][j]:
                 play(i, j)
 
-    print(max(res))
+
+    print(f'#{test_case}', max(res))
