c574ef6b5f2f316d9a630c61d7550b01aff19f4c
chanwoong114 <cksdnd10001@gmail.com>
2023-03-13 00:17:06 +0900

    23/03/13-home

--- 구현/2048(Easy).py
+++ 구현/2048(Easy).py
@@ -1,14 +1,12 @@
 from copy import deepcopy
 
 def move(a, d, board):
-    flag = 0
     if d == 0:
         for i in range(a):
             for j in range(a-1, 0, -1):
                 if not board[i][j] or board[i][j] == board[i][j-1]:
                     board[i][j] += board[i][j-1]
                     board[i][j-1] = 0
-                    flag = 1
 
     elif d == 1:
         for i in range(a):
@@ -16,7 +14,6 @@ def move(a, d, board):
                 if not board[i][j] or board[i][j] == board[i][j+1]:
                     board[i][j] += board[i][j+1]
                     board[i][j+1] = 0
-                    flag = 1
 
     elif d == 2:
         for i in range(a):
@@ -24,7 +21,6 @@ def move(a, d, board):
                 if not board[j][i] or board[j][i] == board[j-1][i]:
                     board[j][i] += board[j-1][i]
                     board[j-1][i] = 0
-                    flag = 1
 
     elif d == 3:
         for i in range(a):
@@ -32,19 +28,17 @@ def move(a, d, board):
                 if not board[j][i] or board[j][i] == board[j+1][i]:
                     board[j][i] += board[j+1][i]
                     board[j+1][i] = 0
-                    flag = 1
-
-    return flag, board
 
 def dfs(a, d, lst):
     global v
-    flag, lst1 = move(n, d, lst)
-    if not flag:
-        return
+    lst1 = deepcopy(lst)
+    move(n, d, lst1)
 
     if a == 5:
         for k in lst1:
             v = max(v, max(k))
+            print(*k)
+        print()
         return
 
     for k in range(4):
@@ -55,6 +49,8 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 v = 0
 
 for ii in range(4):
-    dfs(1, ii, arr)
+    arr1 = deepcopy(arr)
+    dfs(1, ii, arr1)
 
 print(v)
+
