4bcf517643abd4673f03b4acc59f59f8c10943ce
chanwoong114 <cksdnd10001@gmail.com>
2023-03-15 00:51:47 +0900

    23/03/14-home

--- preboard.py
+++ preboard.py
@@ -1,27 +1,28 @@
-board = [[1, 1, 0, 0, 0], [1, 0, 1, 0, 2], [1, 1, 1, 1, 2], [0, 1, 1, 1, 4], [0, 0, 0, 0, 0]]
-a = 5
-for i in range(a):
-    cnt = 0
-    for j in range(a):
-        if board[j][i]:
-            for jj in range(j - 1, -1 + cnt, -1):
-                if board[j][i] == board[jj][i]:
-                    board[j][i] = 0
-                    board[jj][i] *= 2
-                    cnt += 1
-                    break
-
-                elif board[jj][i] and board[j][i] != board[jj][i]:
-                    cnt += 1
-                    save = board[j][i]
-                    board[j][i] = 0
-                    board[jj + 1][i] = save
-                    break
-
-                elif jj == cnt:
-                    save = board[j][i]
-                    board[j][i] = 0
-                    board[jj][i] = save
+from collections import deque
+from sys import stdin
+def bfs(y, x):
+    q = deque()
+    q.append((y, x))
+    lst[y][x] = 0
+    while q:
+        y, x = q.popleft()
+        for dy, dx in [[0, 1], [0, -1], [1, 0], [-1, 0], [-1, -1], [-1, 1], [1, -1], [1, 1]]:
+            ndy = y + dy
+            ndx = x + dx
+            if 0 <= ndy < h and 0 <= ndx < w and lst[ndy][ndx] == 1:
+                lst[ndy][ndx] = 0
+                q.append((ndy, ndx))
 
 
-print(board)
+while True:
+    w, h = map(int, stdin.readline().split())
+    if w == 0 and h == 0:
+        break
+    lst = [list(map(int, stdin.readline().split())) for _ in range(h)]
+    count = 0
+    for i in range(h):
+        for j in range(w):
+            if lst[i][j] == 1:
+                bfs(i, j)
+                count += 1
+    print(count)
