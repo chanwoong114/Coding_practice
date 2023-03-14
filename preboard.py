901d9286a1fc4739d722333342f205c4e19a3bd4
chanwoong114 <chanwoong114>
2023-03-14 17:47:53 +0900

    23/03/14

--- preboard.py
+++ preboard.py
@@ -1,27 +1,2 @@
-board = [[1, 1, 0, 0, 0], [1, 0, 1, 0, 2], [1, 1, 1, 1, 2], [0, 1, 1, 1, 4], [0, 0, 0, 0, 0]]
-a = 5
-for i in range(a):
-    cnt = 0
-    for j in range(a):
-        if board[j][i]:
-            for jj in range(j - 1, -1 + cnt, -1):
-                if board[j][i] == board[jj][i]:
-                    board[j][i] = 0
-                    board[jj][i] *= 2
-                    cnt += 1
-                    break
-
-                elif board[jj][i] and board[j][i] != board[jj][i]:
-                    cnt += 1
-                    save = board[j][i]
-                    board[j][i] = 0
-                    board[jj + 1][i] = save
-                    break
-
-                elif jj == cnt:
-                    save = board[j][i]
-                    board[j][i] = 0
-                    board[jj][i] = save
-
-
-print(board)
+lst = list(map(int, input()))
+print(lst)
---
+++ 구현/주사위_굴리기.py
@@ -0,0 +1,7 @@
+n, m, r, c, t = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+turn = list(map(int, input().split()))
+
+row = [0, 0, 0, 0]
+col = [0, 0, 0, 0]
+
